ad3c21506e2a098b11f051ae9e0be498bd75216c
TheManWhoMorbed <109364438+TheManWhoMorbed@users.noreply.github.com>
2022-07-25 11:12:46 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,3 +1,9 @@
+#Area of Circle
 r = float(input("Please input the radius of the circle: "))
 area=3.14*r*r
 print("The area of the circle with given radius is: ",area)
+
+#Extension of the File
+filename = input("Enter Filename: ")
+ext = filename.split(".")
+print ("Extension of the file is : " + ext[-1])
