53b3167c377ddb2ae2b0b2d8f69af2df00025558
TheManWhoMorbed <109364438+TheManWhoMorbed@users.noreply.github.com>
2022-07-31 11:42:40 +0530

    Update Fibonacci Numbers.py

--- Fibonacci Numbers.py
+++ Fibonacci Numbers.py
@@ -1,3 +1,4 @@
+#Program for Fibonacci Series
 inp=int(input("How many terms? "))
 n=0
 n1=1
@@ -6,3 +7,10 @@ for i in range(inp):
     print(n)
     n=n1
     n1=prv
+#Program for finding positive values in a list
+list1=[-1,2,3,-6,-45,12]
+list2=[]
+for i in range(len(list1)):
+    if list1[i]>0:
+        list2.insert(i,list1[i])
+print(list2)
